066534f6a53df0f78f2885fee6089b5ec836a611
Sheldon Smickley <sheldon.smickley@gmail.com>
2023-04-21 23:56:04 -0400

    adjust title

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -30,7 +30,7 @@ export const HomeView: FC = ({}) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-6xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          Shill Your Twitter Banner
+          Shill Your Banner
           <span className="text-sm font-normal align-top text-slate-700"></span>
         </h1>
         <h4 className="md:w-full text-center text-slate-300 my-2">
